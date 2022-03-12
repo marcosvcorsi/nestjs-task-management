48dfe12581bd082187efd43dd80cab76ae62d0fb
Marcos Corsi <marcosvcorsi@gmail.com>
2022-03-11 21:58:49 -0300

    feat: add task filter validation

--- src/tasks/dto/filter-tasks.dto.ts
+++ src/tasks/dto/filter-tasks.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class FilterTasksDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
   search?: string;
 }
