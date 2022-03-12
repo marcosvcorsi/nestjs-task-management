39ef564680d1a0f2fc19f4d7aedd084d496e0746
Marcos Corsi <marcosvcorsi@gmail.com>
2022-03-11 21:44:41 -0300

    feat: add validation to get task by id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -24,7 +24,13 @@ export class TasksService {
   }
 
   getById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+
+    return task;
   }
 
   create({ title, description }: CreateTaskDto): Task {
@@ -41,6 +47,8 @@ export class TasksService {
   }
 
   delete(id: string): void {
+    this.getById(id);
+
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
