a8dff26bd4ef06f0d1179120e91f5ff63f43f08e
Marcos Corsi <marcosvcorsi@gmail.com>
2022-03-12 18:05:20 -0300

    feat: add typeorm and connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'docker',
+      database: 'tasks_management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TasksModule,
+  ],
 })
 export class AppModule {}
