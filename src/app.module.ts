9bb46e7f688c531ee6a5ce723e2c93223d9e5332
Marcos Corsi <marcosvcorsi@gmail.com>
2022-03-17 21:42:54 -0300

    feat: add auth module and structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { TasksModule } from './tasks/tasks.module';
       synchronize: true,
     }),
     TasksModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
