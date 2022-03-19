7551976ed5bf187e3a9f83e67fb655766ad1c9d6
Marcos Corsi <marcosvcorsi@gmail.com>
2022-03-19 20:53:43 -0300

    feat: add signup

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,15 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser({ username, password }: AuthCredentialsDto): Promise<void> {
+    const user = this.create({
+      username,
+      password,
+    });
+
+    await this.save(user);
+  }
+}
