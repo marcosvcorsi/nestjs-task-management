618b2344d07d03884b3d2fff9e6111a4502e84d2
Marcos Corsi <marcosvcorsi@gmail.com>
2022-03-27 10:37:22 -0300

    feat: add logger example

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
 export class AuthController {
+  private logger = new Logger(AuthController.name);
+
   constructor(private readonly authService: AuthService) {}
 
   @Post('/sign-up')
   async signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    this.logger.verbose(
+      `User "${authCredentialsDto.username}" is trying to sign up`,
+    );
+
     return this.authService.signUp(authCredentialsDto);
   }
 
@@ -15,6 +21,10 @@ export class AuthController {
   async signIn(
     @Body() authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
+    this.logger.verbose(
+      `User "${authCredentialsDto.username}" is trying to sign in`,
+    );
+
     return this.authService.signIn(authCredentialsDto);
   }
 }
