e1a505948abfe441318541636e7e0c734afdbd96
Marcos Corsi <marcosvcorsi@gmail.com>
2022-04-01 20:48:41 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
 
